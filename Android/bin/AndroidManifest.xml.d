48d255cf2c358c28bd022c05b57ac7900ffc9300
David <david@david-VirtualBox.(none)>
2013-05-09 04:08:55 -0700

    build by ant

---
+++ Android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/david/tmp/CS185C-CS286/Android/bin/AndroidManifest.xml : \
+/home/david/tmp/CS185C-CS286/Android/AndroidManifest.xml \
---
+++ Android/bin/Cinequest-debug-unaligned.apk.d
@@ -0,0 +1,7 @@
+/home/david/tmp/CS185C-CS286/Android/bin/Cinequest-debug-unaligned.apk : \
+/home/david/tmp/CS185C-CS286/Android/bin/Cinequest.ap_ \
+/home/david/tmp/CS185C-CS286/Android/bin/classes.dex \
+/home/david/tmp/CS185C-CS286/JavaCommon/bin/classes.jar \
+/home/david/tmp/CS185C-CS286/zxing/bin/classes.jar \
+/home/david/adt-bundle-linux-x86/sdk/tools/support/annotations.jar \
+/home/david/tmp/CS185C-CS286/zxing/libs/core.jar \
