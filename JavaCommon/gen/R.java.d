9452077e8c4ccf10f68333c35b8da4b2d46b9e23
David <david@david-VirtualBox.(none)>
2013-05-09 04:32:08 -0700

    add core.jar

---
+++ JavaCommon/gen/R.java.d
@@ -0,0 +1,4 @@
+/home/david/tmp/CS185C-CS286/JavaCommon/gen/edu/sjsu/cinequest/R.java \
+ : /home/david/tmp/CS185C-CS286/JavaCommon/res/values/strings.xml \
+/home/david/tmp/CS185C-CS286/JavaCommon/res/layout/main.xml \
+/home/david/tmp/CS185C-CS286/JavaCommon/bin/AndroidManifest.xml \
---
+++ zxing/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/david/tmp/CS185C-CS286/zxing/bin/AndroidManifest.xml : \
+/home/david/tmp/CS185C-CS286/zxing/AndroidManifest.xml \
